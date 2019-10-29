8afeffd193cbfe3af7870337f7c6376f42943927
juanam8 <juanam9k@gmail.com>
2019-10-29 09:48:08 +0100

    Estructura sin funcionamiento de FSM de Student y MessyStudent (solo Debug.Log) + Asset placehodler de los personajes.

--- Assets/Scripts/MessyStudent.cs
+++ Assets/Scripts/MessyStudent.cs
@@ -5,16 +5,58 @@ using UnityEngine;
 public class MessyStudent : Student
 {
     //parameters
-
+    protected enum messStates
+    {
+        lookForMess, sabotageDrink, negotiateOrganizer, runAway,
+        botherTeacher, checkAffinity, fightStudent
+    }
+    protected messStates currentMessState;
 
     //methods
     public MessyStudent(string name, Genders gender, Vector2 position) : base(name, gender, position)
     {
         this.role = Roles.MessyStudent;
+        this.currentMessState = messStates.lookForMess;
     }
 
     public override void Trouble()
     {
         Debug.Log("[" + name + "] Ni**er");
+        
+        //Messy Student's FSM
+        switch (currentMessState)
+        {
+            case messStates.lookForMess:
+                Debug.Log("[" + name + "] Looking for some trouble...");
+                this.currentMessState = messStates.sabotageDrink;
+                break;
+            case messStates.sabotageDrink:
+                Debug.Log("[" + name + "] Drinking should be fun!");
+                this.currentMessState = messStates.botherTeacher;
+                break;
+            case messStates.botherTeacher:
+                Debug.Log("[" + name + "] Hey, teacher! Leave those kids alone!");
+                this.currentMessState = messStates.checkAffinity;
+                break;
+            case messStates.checkAffinity:
+                Debug.Log("[" + name + "] Is this one chad or virgin?");
+                this.currentMessState = messStates.fightStudent;
+                break;
+            case messStates.fightStudent:
+                Debug.Log("[" + name + "] Take this Billy!");
+                this.currentMessState = messStates.negotiateOrganizer;
+                break;
+            case messStates.negotiateOrganizer:
+                Debug.Log("[" + name + "] Don't sneak!");
+                this.currentMessState = messStates.runAway;
+                break;
+            case messStates.runAway:
+                Debug.Log("[" + name + "] Run run run!");
+                this.currentMessState = messStates.sabotageDrink;
+                break;
+            default:
+                break;
+        }
     }
+
 }
--- Assets/Scripts/Student.cs
+++ Assets/Scripts/Student.cs
@@ -5,15 +5,123 @@ using UnityEngine;
 public class Student : Character
 {
     //parameters
+    protected enum studentStates {start, roleBehaviour, rest, drink, breath, punishment}
+    protected enum drinkStates { walkToBar, waitQueue, drinking}
+    protected enum restStates { walkToBenches, satInBench}
+    protected enum breathStates { walkOutside, stayOutside}
+    protected enum punishmentStates { waitEndOfPunishment, scapeFromPunishment}
+
+    protected studentStates currentState;
+    protected drinkStates currentDrink;
+    protected restStates currentRest;
+    protected breathStates currentBreath;
+    protected punishmentStates currentPunishment;
 
     //methods
     protected Student(string name, Genders gender, Vector2 position) : base(name, gender, position)
     {
-        
+        this.currentState = studentStates.start;
+        this.currentDrink = drinkStates.walkToBar;
+        this.currentRest = restStates.walkToBenches;
+        this.currentBreath = breathStates.walkOutside;
+        this.currentPunishment = punishmentStates.waitEndOfPunishment;
     }
 
     public override void Enjoying()
     {
         Debug.Log("[" + name + "] I'm having fun!");
+
+        //Student FSM
+        switch (currentState)
+        {
+            case studentStates.start:
+                Debug.Log("[" + name + "] Changing to my behaviour state");
+                break;
+            case studentStates.breath:
+                Debug.Log("[" + name + "] Breathing state");
+                break;
+            case studentStates.drink:
+                Debug.Log("[" + name + "] Drinking state");
+                break;
+            case studentStates.punishment:
+                Debug.Log("[" + name + "] Punishment state");
+                break;
+            case studentStates.rest:
+                Debug.Log("[" + name + "] Resting state");
+                break;
+            case studentStates.roleBehaviour:
+                Debug.Log("[" + name + "] This is my special behaviour");
+                break;
+            default:
+                break;
+        }
     }
+
+    //Drinking State FSM: Messy and Calm Students
+    protected void Drinking()
+    {
+        switch (currentDrink)
+        {
+            case drinkStates.walkToBar:
+                Debug.Log("[" + name + "] Walking to the bar...");
+                break;
+            case drinkStates.waitQueue:
+                Debug.Log("[" + name + "] Waiting queue...");
+                break;
+            case drinkStates.drinking:
+                Debug.Log("[" + name + "] Actually drinking!");
+                break;
+            default:
+                break;
+        }
+    }
+
+    //Resting State FSM: Messy and Calm Students
+    protected void Resting()
+    {
+        switch (currentRest)
+        {
+            case restStates.walkToBenches:
+                Debug.Log("[" + name + "] Walking to benches");
+                break;
+            case restStates.satInBench:
+                Debug.Log("[" + name + "] Sat in the bench");
+                break;
+            default:
+                break;
+        }
+    }
+
+    //Breathing State FSM: Messy and Calm Students
+    protected void Breathing()
+    {
+        switch (currentBreath)
+        {
+            case breathStates.walkOutside:
+                Debug.Log("[" + name + "] Walking outside...");
+                break;
+            case breathStates.stayOutside:
+                Debug.Log("[" + name + "] I'm outside!");
+                break;
+            default:
+                break;
+        }
+    }
+
+    //Punishment State FSM: Messy and Calm Students
+    protected void Punishment()
+    {
+        switch (currentPunishment)
+        {
+            case punishmentStates.waitEndOfPunishment:
+                Debug.Log("[" + name + "] Waiting for being free...");
+                break;
+            case punishmentStates.scapeFromPunishment:
+                Debug.Log("[" + name + "] Now it's my chance!");
+                break;
+            default:
+                break;
+        }
+    }
+    
 }
