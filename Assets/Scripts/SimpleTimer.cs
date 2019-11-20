764462893996c7185bd3fbdad42da15e41a935dc
Rinkashiki <55148458+Rinkashiki@users.noreply.github.com>
2019-11-20 23:53:53 +0100

    Revert "Revert "Timer y gustos musicales""
    This reverts commit 0c204e8e49e5b4827bb5479fa8724558552d6e79.

---
+++ Assets/Scripts/SimpleTimer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleTimer 
+{
+    private float count;
+    private float runningCount;
+    private bool isStarted;
+
+    public SimpleTimer(float count)
+    {
+        this.count = count;
+        runningCount = count;
+        isStarted = false;
+}
+    // Update is called once per frame
+    public void Update()
+    {
+        if (!isFinished() && isStarted)
+        { 
+            runningCount -= Time.deltaTime;
+        } else if (isFinished())
+        {
+            isStarted = false;
+        }
+    }
+
+    public bool isFinished()
+    {
+       if(runningCount > 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    public void start()
+    {
+        isStarted = true;
+    }
+
+    public void reset()
+    {
+        runningCount = count;
+        start();
+    }
+
+}
