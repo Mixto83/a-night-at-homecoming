01ba7ddbf6af7a49dae8bbc467e86f4e85aa9140
César Carbajo <cesares10@gmail.com>
2019-11-24 16:54:48 +0100

    Escena de menu añadida con el boton y el logo de juan
    Co-Authored-By: juanam8 <juanam8@users.noreply.github.com>

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuController: MonoBehaviour
+{
+    public void NextScene()
+    {
+        SceneManager.LoadScene("TestScene");
+    }
+}
